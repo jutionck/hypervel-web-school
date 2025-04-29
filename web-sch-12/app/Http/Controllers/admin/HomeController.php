622fa2c3701787847d388078d333878a4da8bcac
Gugun Gunawan <gugun.net21@gmail.com>
2025-04-29 23:58:02 +0700

    slicing admin di laravel 12

--- web-sch-12/app/Http/Controllers/admin/HomeController.php
+++ web-sch-12/app/Http/Controllers/admin/HomeController.php
@@ -1,13 +1,15 @@
 <?php
-
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function indexView()
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    public function indexPage()
     {
         return view('home');
     }
--- web-sch-12/routes/web.php
+++ web-sch-12/routes/web.php
@@ -6,4 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/', [HomeController::class, 'indexView']);
+Route::get('/home', [HomeController::class, 'indexPage'])->name('home');
+Auth::routes();
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
