da32e12ba9b72b8df7e5a50647999fcb51f24421
Gugun Gunawan <gugun.net21@gmail.com>
2025-04-29 23:42:16 +0700

    slicing ui admin

--- web-sch-12/routes/web.php
+++ web-sch-12/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\admin\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/', [HomeController::class, 'indexView']);
