0459bae541524a1168a2d0f0e5162732b30d89b2
Gugun Gunawan <gugun.net21@gmail.com>
2025-05-10 17:07:48 +0700

    install middleware, kernel

---
+++ web-sch-12/app/Http/Controllers/admin/UserController.php
@@ -0,0 +1,38 @@
+<?php
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+
+class UserController extends Controller
+{
+    public function indexview()
+    {
+
+    }
+
+    public function index()
+    {
+
+    }
+    public function create()
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+    public function edit($id)
+    {
+
+    }
+    public function update($id)
+    {
+
+    }
+    public function destroy($id)
+    {
+
+    }
+}
--- web-sch-12/routes/web.php
+++ web-sch-12/routes/web.php
@@ -2,11 +2,14 @@
 
 use App\Http\Controllers\admin\HomeController;
 use App\Http\Controllers\frontend\FeController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [FeController::class, 'indexFe'])->name('landingpage');
 
-Route::get('/home', [HomeController::class, 'indexPage'])->name('home');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/home', [HomeController::class, 'indexPage'])->name('home');
+});
 Auth::routes();
 
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
