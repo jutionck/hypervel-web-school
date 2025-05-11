4307dca06679067b6ddecd810e8f0ea0d75926dc
Gugun Gunawan <gugun.net21@gmail.com>
2025-05-11 11:35:30 +0700

    call new asset font, admin common, asset juqery, data tble,

--- web-sch-12/resources/views/admin/layouts/app.blade.php
+++ web-sch-12/resources/views/admin/layouts/app.blade.php
@@ -6,7 +6,9 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="shortcut icon" href="assets/images/favicon.svg" type="image/x-icon" />
+    <link rel="shortcut icon" href="backend/assets/images/favicon.svg" type="image/x-icon" />
+    <link rel="stylesheet" href="{{ asset('backend/assets/css/font-awesome.6.4.0.min.css') }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title')</title>
 
     <!-- ========== All CSS files linkup ========= -->
@@ -15,6 +17,8 @@
 </head>
 
 <body>
+    <!--alert session-->
+    @include('components.alert-session')
     <!-- ======== Preloader =========== -->
     <div id="preloader">
         <div class="spinner"></div>
@@ -25,7 +29,7 @@
     <aside class="sidebar-nav-wrapper">
         <div class="navbar-logo">
             <a href="index.html">
-                <img src="assets/images/logo/logo.svg" alt="logo" />
+                <img src="backend/assets/images/logo/logo.svg" alt="logo" />
             </a>
         </div>
         @include('admin.includes.sidebar')
@@ -51,6 +55,10 @@
 
     <!-- ========= All Javascript files linkup ======== -->
     @include('admin.includes.scripts')
+    <!-- ========= All Javascript files linkup ======== -->
+    <!-- ========== admin common script ========== -->
+    <script src="{{ asset('backend/assets/js/admin-common.js') }}"></script>
+    <!-- ========== admin common script ========== -->
 
 </body>
 
